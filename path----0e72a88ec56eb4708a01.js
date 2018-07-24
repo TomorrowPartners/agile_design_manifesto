b56c4f3732015766c470095720b1f33d46dfd8ce
Kirill Mazin <kirill@40.local>
2018-07-23 18:03:16 -0700

    Updates

--- path----0e72a88ec56eb4708a01.js
+++ path----0e72a88ec56eb4708a01.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{133:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Manifesto for Agile Design and Development"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{105:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Manifesto for Agile Design and Development"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----0e72a88ec56eb4708a01.js.map
